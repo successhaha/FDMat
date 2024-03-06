20682e36b97df62e54a882fc9a60e2603e136f04
successhaha <59720319+successhaha@users.noreply.github.com>
2024-03-06 11:03:23 +0800

    Update fdmat.py

--- deepcore/methods/fdmat.py
+++ deepcore/methods/fdmat.py
@@ -5,9 +5,9 @@ from ..nets.nets_utils import MyDataParallel
 import collections
 from torch import tensor, long
 def compute_optimal_transport(M, r, c, epsilon=1e-6, lam=4):    
-        r = r.cuda() # 5 50000
-        c = c.cuda() # 5 10
-        n_runs, n, m = M.shape  # 5, 50000, 10
+        r = r.cuda()
+        c = c.cuda()
+        n_runs, n, m = M.shape  
         P = torch.exp(- lam * M)
         P /= P.view((n_runs, -1)).sum(1).unsqueeze(1).unsqueeze(1)                        
         u = torch.zeros(n_runs, n).cuda()
@@ -21,7 +21,7 @@ def compute_optimal_transport(M, r, c, epsilon=1e-6, lam=4):
             if iters == maxiters:
                 break
             iters = iters + 1
-        return P, P*M #torch.sum(P * M)
+        return P, P*M 
 
 class fdmat(EarlyTrain):
     def __init__(self, dst_train, args, fraction=0.5, random_seed=None, epochs=200, repeat=5,
@@ -35,7 +35,6 @@ class fdmat(EarlyTrain):
         self.dst_val = dst_val
 
         self.balance = balance
-        # print("| 2Fraction[%.1f] Epochs[%3d]" % (fraction, epochs))
 
     def while_update(self, outputs, loss, targets, epoch, batch_idx, batch_size):
         if batch_idx % self.args.print_freq == 0:
@@ -59,70 +58,37 @@ class fdmat(EarlyTrain):
         dataset["labels"] = torch.LongTensor(np.concatenate(labels))
         return dataset
 
-    def define_data(self, val): 
+    def define_data(self, val): # Define data format
         self.model.embedding_recorder.record_embedding = True
         self.model.eval()
-        # if you use validation sets (the default is False)
-        if val:
-            val_batch_loader = torch.utils.data.DataLoader(self.dst_val, batch_size=self.args.selection_batch,
-                                                           num_workers=self.args.workers)
-            train_batch_loader = torch.utils.data.DataLoader(self.dst_train, batch_size=self.args.selection_batch,
+        train_batch_loader = torch.utils.data.DataLoader(self.dst_train, batch_size=self.args.selection_batch,
                                                              num_workers=self.args.workers)
-            with torch.no_grad():
-                output_dict = collections.defaultdict(list)
-                j = 0
-                for i, (inputs, labels) in enumerate(val_batch_loader):
-                    inputs = inputs.cuda()
-                    labels = labels.cuda()
-                    outputs, _ = self.model(inputs)
+        save_features = []
+        save_labels = []
+        with torch.no_grad():
+            output_dict = collections.defaultdict(list)
+            j = 0
+            # loading train samples
+            for i, (inputs, labels) in enumerate(train_batch_loader):
+                inputs = inputs.to(self.args.device)
+                labels = labels.to(self.args.device)
+                outputs = self.model(inputs)
+                # getting the feature embedding of the last layer 
+                if self.args.approximate:
                     outputs = outputs.cpu().data.numpy()
                     for out, label in zip(outputs, labels):
                         indx_feature = [j, out]
                         j += 1
                         output_dict[label.item()].append(indx_feature)
-                k = 0
-                for i, (inputs, labels) in enumerate(train_batch_loader):
-                    inputs = inputs.to(self.args.device)
-                    labels = labels.to(self.args.device)
-                    _ = self.model(inputs)
+                else:
                     outputs = self.model.embedding_recorder.embedding
                     outputs = outputs.cpu().data.numpy()
                     for out, label in zip(outputs, labels):
-                        indx_feature = [j + k, out]
-                        k += 1
+                        indx_feature = [j, out]
+                        save_features.append(out)
+                        save_labels.append(int(label.item()))
+                        j += 1
                         output_dict[label.item()].append(indx_feature)
-        else:
-            train_batch_loader = torch.utils.data.DataLoader(self.dst_train, batch_size=self.args.selection_batch,
-                                                             num_workers=self.args.workers)
-            save_features = []
-            save_labels = []
-            with torch.no_grad():
-                output_dict = collections.defaultdict(list)
-                j = 0
-                # loading training samples
-                # for k in range(1):
-                    for i, (inputs, labels) in enumerate(train_batch_loader):
-                        inputs = inputs.to(self.args.device)
-                        labels = labels.to(self.args.device)
-                        _ = self.model(inputs)
-                        # getting the feature embedding of the last layer 
-                        if self.args.approximate:
-                            outputs = _
-                            outputs = outputs.cpu().data.numpy()
-                            for out, label in zip(outputs, labels):
-                                indx_feature = [j, out]
-                                j += 1
-                                output_dict[label.item()].append(indx_feature)
-                        else:
-                            outputs = self.model.embedding_recorder.embedding
-                            outputs = outputs.cpu().data.numpy()
-                            for out, label in zip(outputs, labels):
-                                indx_feature = [j, out]
-                                if k==0:
-                                    save_features.append(out)
-                                    save_labels.append(int(label.item()))
-                                j += 1
-                                output_dict[label.item()].append(indx_feature)
         self.model.train()
         self.model.embedding_recorder.record_embedding = False
 
@@ -142,14 +108,13 @@ class fdmat(EarlyTrain):
             classes_name.append(int(dataset["labels"][int(indices[0])]))
             indices = torch.where(labels != labels[0])[0]
             labels = labels[indices]
-        return   save_each_data, save_each_data_class
-    def proccess_data(self):
-        new_datas, all_data_index = self.define_data(val=False)  # ndatas-->10, 10000, 512
+        return save_each_data, save_each_data_class
+    def proccess_data(self): # 
+        new_datas, all_data_index = self.define_data(val=False)  
         # Power transform
         for i in range(len(new_datas)):
             beta = 0.5
             new_datas[i] = torch.pow(new_datas[i]+1e-6, beta)
-            # new_datas[i] = new_datas/new_datas.norm(dim=2, keepdim=True)
         # CenterDatas
         cat_datas = torch.cat(new_datas, dim=0)
         new_datas_mean = cat_datas.mean(0, keepdim=True)
@@ -165,11 +130,9 @@ class fdmat(EarlyTrain):
         for i in range(len(new_datas)):
             new_datas[i] = ((cat_datas[new_datas_indices[i]:new_datas_indices[i+1],:] - new_datas_mean)/new_datas_norm[new_datas_indices[i]:new_datas_indices[i+1],:]) #(10000, 1)
             data_mus.append(new_datas[i].mean(0).unsqueeze(0))
-        data_mus = torch.cat(data_mus, dim=0)  #(10, 512)
-
-
+        data_mus = torch.cat(data_mus, dim=0)  
         datas = torch.cat(new_datas, dim=0).unsqueeze(0)
-        mus = data_mus.unsqueeze(0) #(1,10,512)
+        mus = data_mus.unsqueeze(0) 
 
         # define cost matrix
         all_samples = datas.size()[1]
@@ -179,21 +142,21 @@ class fdmat(EarlyTrain):
             dist[:,i*int(all_samples/num_class):(i+1)*int(all_samples/num_class),:] = (datas[:,i*int(all_samples/num_class):(i+1)*int(all_samples/num_class),].unsqueeze(2)-mus.unsqueeze(1)).norm(dim=3).pow(2)
         n_runs = 1
         n_usamples = int(all_samples)
-        # Optimal Transport
+            
+        # select the coreset by uisng optimal transport
         r = torch.ones(n_runs, n_usamples) 
         c = torch.ones(n_runs, num_class) * torch.tensor(class_indices)
         p_xj_test, all_sum = compute_optimal_transport(dist[:, :], r, c, epsilon=1e-6, lam=self.args.lmda)
         result = all_sum.norm(dim=2).pow(2)[0]
         return result
 
-    def finish_run(self):  # train a few epochs to get a pre-trained model and to select coreset
+    def finish_run(self):  # get the pre-trained model by training a few epoches and select coreset
         self.model.embedding_recorder.record_embedding = True  # recording embedding vector
         self.model.eval()
         b, all_index= self.define_data(val=False)
         result = self.proccess_data()
         return result, all_index
     def select(self, **kwargs):
-        # Initialize a matrix to save norms of each sample on idependent runs
         self.run()
         result,  all_index = self.finish_run()
         self.train_indx = np.arange(self.n_train)
